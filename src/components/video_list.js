00cc98e147de34b56233588de0be8915f0b6fb45
Raymond Zhu <ruzhu@syr.edu>
2017-07-23 22:29:20 -0400

    Finished my first react

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,11 +1,17 @@
 import React from 'react';
-
+import VideoListItem from './video_list_item';
 
 const VideoList = (props) => {
-  const videos = props.videos;
+  const VideoList = props.videos.map((video) => {
+    return <VideoListItem
+      onVideoSelect={props.onVideoSelect}
+      key={video.etag}
+      video={video}/>
+  });
+
   return (
     <ul className="col-md-4 list-group">
-      {props.videos.length}
+      {VideoList}
     </ul>
   )
 };
